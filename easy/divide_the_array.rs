f07e4e2557bd747058a692accc552bf2c70cc72a
Jorge Luna <seguidor777@gmail.com>
2020-01-31 09:21:41 -0600

    Divide The Array

--- easy/divide_the_array.rs
+++ easy/divide_the_array.rs
@@ -1,24 +1,5 @@
 use std::io;
 
-// Find the largest gap
-fn max_gap(v: &Vec<i32>, v_len: usize) -> i32 {
-    let (mut gap, mut max): (i32, i32);
-
-    max = 0;
-
-    for i in 0..(v_len - 1) {
-        for j in 1..v_len {
-            gap = i32::abs(v[i] - v[j]);
-
-            if gap > max {
-                max = gap
-            }
-        }
-    }
-
-    max
-}
-
 fn main() {
     let mut input = String::new();
 
@@ -37,17 +18,27 @@ fn main() {
         .split_whitespace()
         .filter_map(|x| x.parse().ok())
         .collect();
+    let mut evens: Vec<i32> = vec![];
 
     // Constraints
-    if n >= 2 && n <= 10000 {
-        for item in &v {
-            if *item < 1 || *item > 100000 {
+    if n >= 1 && n <= 10000 {
+        for (i, item) in v.iter().enumerate() {
+            if *item < 1 || *item > 1000000 {
                 return;
+            }
+
+            if (i + 1) % 2 == 0 && *item % 2 == 0 {
+                evens.push(*item);
             }
         }
 
-        let max = max_gap(&v, v.len());
-
-        println!("{}", max)
+        println!(
+            "{}",
+            evens
+                .iter()
+                .map(ToString::to_string)
+                .collect::<Vec<String>>()
+                .join(" ")
+        )
     }
 }

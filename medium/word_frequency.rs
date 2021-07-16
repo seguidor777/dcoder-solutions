7efa1353a9d463c980b062fc70345a1ceb275305
Jorge Luna <seguidor777@gmail.com>
2021-07-16 16:56:25 -0400

    Word Frequency

---
+++ medium/word_frequency.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("cannot read input");
+
+    input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("cannot read input");
+
+    let words_input: Vec<&str> = input.split_whitespace().collect();
+    let mut words_counter: HashMap<&str, u32> = HashMap::new();
+    let mut sortable: Vec<&str> = Vec::new();
+
+    for word in words_input.into_iter() {
+        if let Some(count) = words_counter.get_mut(word) {
+            *count += 1;
+            continue;
+        }
+
+        words_counter.insert(word, 1);
+        sortable.push(word);
+    }
+
+    sortable.sort_by(|a, b| words_counter[b].cmp(&words_counter[a]));
+    sortable.dedup();
+    println!("{}", sortable.join(" "));
+}

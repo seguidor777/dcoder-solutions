0f3fa55512fb3caf0c2da04d2a2dc68f247d642a
Jorge Luna <seguidor777@gmail.com>
2021-08-03 20:05:24 -0400

    Power Number

---
+++ medium/power_number.rs
@@ -0,0 +1,38 @@
+use std::io::{self, BufRead};
+
+fn is_power(n: u64) -> bool {
+    let mut pow: u64 = 1;
+    let mut result: u64;
+
+    loop {
+        result = pow.pow(pow as u32);
+        if result >= n { break }
+        pow += 1;
+    }
+
+    result == n
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+    let mut input = lines.next().unwrap().expect("cannot read input");
+    let n: u8 = input.trim().parse().expect("cannot parse input");
+
+    input = lines.next().unwrap().expect("cannot read input");
+
+    let integers: Vec<u64> = input
+        .split_whitespace()
+        .filter_map(|x| x.parse().ok())
+        .collect();
+    let mut answers: Vec<&str> = Vec::new();
+
+    for i in 0..n {
+        match is_power(integers[i as usize]) {
+            true => answers.push("Yes"),
+            false => answers.push("No"),
+        }
+    }
+
+    println!("{}", answers.join(" "));
+}
